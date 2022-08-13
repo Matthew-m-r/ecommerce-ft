7e389dc33e5bd91a0b6eb621f0111e05c517df1c
Mateo <teo-m.r@hotmail.com>
2022-08-13 17:43:46 -0500

    [FEATURE] Home Styles Fixings

--- ecommerce/src/components/pages/home/home.component.tsx
+++ ecommerce/src/components/pages/home/home.component.tsx
@@ -1,15 +1,22 @@
 import ItemListContainer from "@components/item-list-container/itemListContainer.component";
-import { useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { categories} from "@mocks/categories";
 import "./home.styles.scss";
+import { useEffect, useState } from "react";
 
 const Home = () => {
+  const [currentCategory, setCurrentCategory] = useState<string>();
   const { categoryId } = useParams();
   
+  useEffect(() => {
+    setCurrentCategory(categories.find(cat => cat.id === categoryId)?.name);
+  }, [categoryId])
+  
+
   return (
     <div className="main-home-container">
       <div className="filter-section">
-        <p>Filtrar por...</p>
+        <h1>{!currentCategory ? "Ofertas del día" : currentCategory}</h1>
       </div>
       <div className="item-list">
         <ItemListContainer categoryId={categoryId} />
--- ecommerce/src/mocks/categories.ts
+++ ecommerce/src/mocks/categories.ts
@@ -1,6 +1,22 @@
 export const categories = [
   {
-    id: 0,
-    name: "",
+    id: "0",
+    name: "Ofertas del día",
+  },
+  {
+    id: "1",
+    name: "Tecnología",
+  },
+  {
+    id: "2",
+    name: "Deporte",
+  },
+  {
+    id: "3",
+    name: "Series",
+  },
+  {
+    id: "4",
+    name: "Mascotas",
   },
 ];
