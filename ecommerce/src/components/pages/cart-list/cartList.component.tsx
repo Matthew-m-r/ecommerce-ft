4b6c16ae49a3d1ccd83bbd6332720dc1fbb9433e
Mateo <teo-m.r@hotmail.com>
2022-08-13 16:35:19 -0500

    [FEATURE] Product Quantity Improves

--- ecommerce/src/components/pages/cart-list/cartList.component.tsx
+++ ecommerce/src/components/pages/cart-list/cartList.component.tsx
@@ -4,6 +4,7 @@ import SubTotalCartItems from "@components/subtotalCartItems/subtotalCartItems.c
 import DropDown from "@components/ui/dropdown/dropdown.component";
 import { useCart } from "@context/cartContext";
 import { ICart } from "@interfaces/cart/cart.interface";
+import { IProduct } from "@interfaces/product/product.interface";
 import { useEffect, useState } from "react";
 import { AiOutlineDelete } from "react-icons/ai";
 import { useNavigate } from "react-router-dom";
@@ -35,10 +36,10 @@ const CartList = () => {
 
   const hanldeDropDownChange = (
     e: React.ChangeEvent<HTMLSelectElement>,
-    productIndex: number
+    product: IProduct
   ) => {
     const quantitySelected = parseInt(e.target.value);
-    setProductQuantity(quantitySelected);
+    addProdToCart({ product: product, quantity: quantitySelected });
   };
 
   const handleBackToShop = () => {
@@ -55,86 +56,91 @@ const CartList = () => {
   return (
     <div className="cart-list-main-container">
       <div className="card-list-main-left-container">
-        <div className="cart-list-description-container">
-          <div className="title-section">
-            {cartProducts.length === 0 ? (
-              <div className="empty-cart">
-                <h1>Tu carrito de Amazon está vacío.</h1>
-                <p>
-                  Tu Carrito de Compras vive para servir. Dale propósito —
-                  llénala con abarrotes, ropa, artículos para el hogar,
-                  electrónicos, y más.{" "}
-                  <span
-                    className="return-home"
-                    onClick={() => handleBackToShop()}
-                  >
-                    Continuar comprando.
-                  </span>
-                </p>
-              </div>
-            ) : (
-              <h1>Tus artículos</h1>
-            )}
+        {!showForm ? (
+          <div className="cart-list-description-container">
+            <div className="title-section">
+              {cartProducts.length === 0 ? (
+                <div className="empty-cart">
+                  <h1>Tu carrito de Amazon está vacío.</h1>
+                  <p>
+                    Tu Carrito de Compras vive para servir. Dale propósito —
+                    llénala con abarrotes, ropa, artículos para el hogar,
+                    electrónicos, y más.{" "}
+                    <span
+                      className="return-home"
+                      onClick={() => handleBackToShop()}
+                    >
+                      Continuar comprando.
+                    </span>
+                  </p>
+                </div>
+              ) : (
+                <h1>Tus artículos</h1>
+              )}
 
-            {cartProducts.length !== 0 && (
-              <AiOutlineDelete onClick={clearCart} />
-            )}
+              {cartProducts.length !== 0 && (
+                <AiOutlineDelete onClick={clearCart} />
+              )}
+            </div>
+
+            {cartProducts
+              ? cartProducts.map((product, productIndex) => {
+                  return (
+                    <div className="cart-product-description">
+                      <div className="image-block">
+                        <img src={product.product.pictureUrl} />
+                      </div>
+                      <div className="description-block">
+                        <div className="product-title-section">
+                          <p>{product.product.title}</p>
+                        </div>
+                        <div className="product-actions-section">
+                          <p
+                            className={`${
+                              product.product?.available
+                                ? "available"
+                                : "unavailable"
+                            }`}
+                          >
+                            Disponible.
+                          </p>
+                          <div className="product-state-section">
+                            <p>Envío Gratis a Colombia.</p>
+                          </div>
+                          <div className="actions-section">
+                            <div className="quantity-section">
+                              <DropDown
+                                name={"Cantidad"}
+                                value={product.product?.stock}
+                                hanldeDropDownChange={(e) =>
+                                  hanldeDropDownChange(e, product.product)
+                                }
+                                defaultValue={product.quantity}
+                              />
+                            </div>
+                            <div className="remove-product">
+                              <p
+                                onClick={() =>
+                                  removeProduct(product.product.id)
+                                }
+                              >
+                                Eliminar
+                              </p>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="price-block">
+                        <h5>USD{priceFormat.format(product.product.price)}</h5>
+                      </div>
+                    </div>
+                  );
+                })
+              : null}
+            <SubTotalCartItems />
           </div>
+        ) : null}
 
-          {cartProducts
-            ? cartProducts.map((product, productIndex) => {
-                return (
-                  <div className="cart-product-description">
-                    <div className="image-block">
-                      <img src={product.product.pictureUrl} />
-                    </div>
-                    <div className="description-block">
-                      <div className="product-title-section">
-                        <p>{product.product.title}</p>
-                      </div>
-                      <div className="product-actions-section">
-                        <p
-                          className={`${
-                            product.product?.available
-                              ? "available"
-                              : "unavailable"
-                          }`}
-                        >
-                          Disponible.
-                        </p>
-                        <div className="product-state-section">
-                          <p>Envío Gratis a Colombia.</p>
-                        </div>
-                        <div className="actions-section">
-                          <div className="quantity-section">
-                            <DropDown
-                              name={"Cantidad"}
-                              value={product.product?.stock}
-                              hanldeDropDownChange={(e) =>
-                                hanldeDropDownChange(e, productIndex)
-                              }
-                              defaultValue={product.quantity}
-                            />
-                          </div>
-                          <div className="remove-product">
-                            <p
-                              onClick={() => removeProduct(product.product.id)}
-                            >
-                              Eliminar
-                            </p>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="price-block">
-                      <h5>USD{priceFormat.format(product.product.price)}</h5>
-                    </div>
-                  </div>
-                );
-              })
-            : null}
-          <SubTotalCartItems />
-        </div>
         {showForm ? (
           <div className="cart-list-description-container main-order-container">
             <OrderForm showForm={showForm} />
